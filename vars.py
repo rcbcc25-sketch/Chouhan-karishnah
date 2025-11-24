2181aa6810a1e9030c8b7873574434766bb3ff4d
rcbcc25-sketch <rcbcc25@gmail.com>
2025-11-25 01:14:03 +0530

    Refactor environment variable configuration

--- vars.py
+++ vars.py
@@ -7,8 +7,8 @@ API_HASH = environ.get("API_HASH", "d0b73a75f2d12cae5b04c21044ff0148")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7698119810:AAGh5Q7Hwa8-a09XUSuuyZPb2mxzsXp4NwY")
 
 # Force Subscribe Configuration
-FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "roxybasicneedbot1")  # Channel username without @, 
-FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/roxybasicneedbot1")  # Channel link
+FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/chaiabah")  # Channel username without @, 
+FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/chaiabah")  # Channel link
 
 # Admin Configuration
 ADMINS = list(map(int, environ.get("ADMINS", "8050673236").split()))
@@ -18,6 +18,7 @@ OWNER_ID = int(environ.get("OWNER_ID", "8050673236"))
 
 # Database URL (if you want to add database support later)
 DATABASE_URL = environ.get("DATABASE_URL", "")
+
 
 
 
